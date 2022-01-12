d61867a0442df35b4603991234e1995ed8239d68
RodrigoVieira06 <rodrigovlima06@gmail.com>
2022-01-12 19:23:16 -0300

    Curso JS Alura - Aula 4 - Adicionando classe de listagem de negociações e e ajustando o código.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,21 +6,31 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
+
         event.preventDefault();
 
-        let data = new Date(
-            ...this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaformulario();
 
-        let negociacao = new Negociacao(
-            data,
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
-        );
+        )
+    }
+
+    _limpaformulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
     }
 }
